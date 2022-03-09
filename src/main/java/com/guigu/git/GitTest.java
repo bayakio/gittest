14ec09a33dd70bb68fe98e7e32458d0ff2205586
bayakio <2697035193@qq.com>
2022-03-09 06:59:24 +0000

    update src/main/java/com/guigu/git/GitTest.java.

--- src/main/java/com/guigu/git/GitTest.java
+++ src/main/java/com/guigu/git/GitTest.java
@@ -3,5 +3,6 @@ package com.guigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git55666565");
+        System.out.println(6666666666);
     }
 }
