6b444b90d0b32727ef77bd0883dacb518641c4f0
bayakio <bayakio@163.com>
2022-03-09 15:01:08 +0800

    gitee commit

--- src/main/java/com/guigu/git/GitTest.java
+++ src/main/java/com/guigu/git/GitTest.java
@@ -2,6 +2,6 @@ package com.guigu.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git55666565");
+        System.out.println("hello git556665651656516");
     }
 }
